5fac4fe32b36294dba6e9f9e1fbab661ac3d012a
SericK <serick@outlook.com>
2013-05-20 16:44:35 -0500

    Formularios Calculo Dias y Prima

---
+++ application/views/prima/calculo_prima.php
@@ -0,0 +1,201 @@
+<div class="row">
+  <div class="span12 offset1">
+  	<form class="form-horizontal">
+  		  <fieldset class="well">
+	  		<div class="control-group span5">
+	    		<label class="control-label" for="registro-patronal-2">Registro Patronal 2</label>
+	    		<div class="controls">
+	      			<select id="registro-patronal-2" name="">
+	      				<option></option>
+	      				<option></option>
+	      				<option></option>
+	      			</select>
+	    		</div>
+	  		</div>
+	  		<div class="control-group span5">
+	    		<label class="control-label" for="registro-patronal-3">Registro Patronal 3</label>
+	    		<div class="controls">
+	      			<select id="registro-patronal-3" name="">
+	      				<option></option>
+	      				<option></option>
+	      				<option></option>
+	      			</select>
+	    		</div>
+	  		</div>
+		<div class="clear"></div>
+	  		<div class="control-group span5">
+	    		<label class="control-label" for="registro-patronal-4">Registro Patronal 4</label>
+	    		<div class="controls">
+	      			<select id="registro-patronal-4" name="">
+	      				<option></option>
+	      				<option></option>
+	      				<option></option>
+	      			</select>
+	    		</div>
+	  		</div>
+
+	  		<div class="control-group span5">
+	    		<label class="control-label" for="registro-patronal-5">Registro Patronal 5</label>
+	    		<div class="controls">
+	      			<select id="registro-patronal-5" name="">
+	      				<option></option>
+	      				<option></option>
+	      				<option></option>
+	      			</select>
+	    		</div>
+	  		</div>
+	  		<div class="clear"></div>
+	  		<div class="control-group span10">
+	    		<label class="control-label" for="registro-patronal-5">Nombre o Razon Social</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="span8" value="">
+	    		</div>
+	  		</div>
+	  		 <div class="control-group span10">
+	    		<label class="control-label" for="registro-patronal-5">Domicilio</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="span8" value="">
+	    		</div>
+	  		</div>
+		   	<div class="control-group span5">
+	    		<label class="control-label" for="registro-patronal-5">Localidad</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="span4" value="">
+	    		</div>
+	  		</div>
+	  		<div class="control-group span4">
+	    		<label class="control-label" for="registro-patronal-5">Telefono</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="span3" value="">
+	    		</div>
+	  		</div>
+	  		<div class="control-group span10">
+	    		<label class="control-label" for="registro-patronal-5">Actividad Economica</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="span8" value="">
+	    		</div>
+	  		</div>
+	  		<div class="control-group span10">
+	    		<label class="control-label" for="registro-patronal-5">Nombre del Patron o Representante Legal</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="span8" value="">
+	    		</div>
+	  		</div>
+	  		<div class="control-group span2">
+	    		<label class="control-label" for="registro-patronal-5">Clase</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="span1" value="">
+	    		</div>
+	  		</div>
+	  		<div class="control-group span3">
+	    		<label class="control-label" for="registro-patronal-5">Fracción</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="span1" value="">
+	    		</div>
+	  		</div>
+	  		<div class="control-group span5">
+	    		<label class="control-label" for="registro-patronal-5">Prima Anterior</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="span3" value="">
+	    		</div>
+	  		</div>
+
+	  	</fieldset>  		  		
+
+  		<fieldset class="well">
+  			<div class="control-group span6 well">
+	    		<label class="control-label label-large" for="registro-patronal-5">Total de casos R.T.</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="input-xmini" value="" disabled>
+	      			<input type="text" id="" name="" class="input-medium" value="">
+	    		</div>
+
+	    		<label class="control-label label-large" for="registro-patronal-5">Total de Dias Subsidiados</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="input-xmini" value="" disabled>	    	
+	      			<input type="text" id="" name="" class="input-medium" value="">
+	    		</div>
+
+	    		<label class="control-label label-large" for="registro-patronal-5">Suma Porc. de Incap. /100</label>
+	    		<div class="controls">
+	    			<input type="text" id="" name="" class="input-xmini" value="" disabled>
+	      			<input type="text" id="" name="" class="input-medium" value="">
+	    		</div>
+
+	    		<label class="control-label label-large" for="registro-patronal-5">N° Defunciones</label>
+	    		<div class="controls">
+	    			<input type="text" id="" name="" class="input-xmini" value="" disabled>
+	      			<input type="text" id="" name="" class="input-medium" value="">
+	    		</div>
+
+	    		<label class="control-label label-large" for="registro-patronal-5">N° de Trab. Prom. Exp. Rgo.</label>
+	    		<div class="controls">
+	    			<input type="text" id="" name="" class="input-xmini" value="" disabled>
+	      			<input type="text" id="" name="" class="input-medium" value="">
+	    		</div>
+				<br/>	
+	    		<label class="control-label label-large" for="registro-patronal-5">Dias Naturales del Año</label>
+	    		<div class="controls">
+	    			<input type="text" id="" name="" class="input-xmini" value="" disabled>
+	      			<input type="text" id="" name="" class="input-medium" value="">
+	    		</div>
+
+	    		<label class="control-label label-large" for="registro-patronal-5">Promedio de Vida Activa</label>
+	    		<div class="controls">
+	    			<input type="text" id="" name="" class="input-xmini" value="" disabled>
+	      			<input type="text" id="" name="" class="input-medium" value="">
+	    		</div>
+
+	    		<label class="control-label label-large" for="registro-patronal-5">Factor de Prima</label>
+	    		<div class="controls">
+	    			<input type="text" id="" name="" class="input-xmini" value="" disabled>
+	      			<input type="text" id="" name="" class="input-medium" value="">
+	    		</div>
+
+	    		<label class="control-label label-large" for="registro-patronal-5">Prima Minima de Riesgos</label>
+	    		<div class="controls">
+	    			<input type="text" id="" name="" class="input-xmini" value="" disabled>
+	      			<input type="text" id="" name="" class="input-medium" value="">
+	    		</div>
+	    		<br>
+	  		</div>
+	
+	<div class="control-group span4 well">
+	    		<label class="control-label" for="registro-patronal-5">Periodo de Computo</label>
+	    		<div class="controls">
+	      			<select class="input-small">
+	      				<option>2013</option>
+	      				<option>2012</option>
+	      				<option>2011</option>
+	      			</select>
+	    		</div>
+	  		<br/>
+				<button class="offset1">Art. 32 Fraccion VII</button>	    		
+	  		</div>
+	  		<div class="control-group span4 well">
+	    		<label class="control-label label-large" for="registro-patronal-5">Acreditación de la ST y PS</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="span1" value="No">
+	    		</div>
+	  		</div>
+	  		<div class="control-group span4 well">
+	    		<label class="control-label" for="registro-patronal-5">Prima Resultante</label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="input-small" value="">
+	    		</div>
+	  		<br/>
+	    		<label class="control-label" for="registro-patronal-5"><b>Prima Nueva</b></label>
+	    		<div class="controls">
+	      			<input type="text" id="" name="" class="input-small" value="">
+	    		</div>
+	  		</div>
+
+    	</fieldset>
+    	<fieldset class="well">  		  	
+	    		<button class="btn span3 offset1">Calcular</button>    			
+	    		<button class="btn span3">Generar Archivo</button>
+	    		<button class="btn span3">Salir</button>
+    	</fieldset>
+	</form>
+  </div>
+</div>

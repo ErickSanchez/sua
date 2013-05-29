d7a49f88f86874d9f8e2b2ccd628e632f90bf9df
Juan Rangel Juarez <jrj_juan@hotmail.com>
2013-05-28 21:46:40 -0500

    ultimo

--- application/views/prima/calculo_dias.php
+++ application/views/prima/calculo_dias.php
@@ -1,4 +1,4 @@
-<div class="row">
+ <div class="row">
   <div class="span12 offset1">
   	<!--form class="form-horizontal"-->
   	<?php echo form_open('',array('class'=>'form-horizontal'));?>
@@ -8,9 +8,56 @@
 	    		<div class="controls">
 	      			<select>
 	      				<option></option>
-	      				<option>2013</option>
-	      				<option>2012</option>
+                        <option>1969</option>
+	      				<option>1970</option>
+                        <option>1971</option>
+	      				<option>1972</option>
+                        <option>1973</option>
+	      				<option>1974</option>
+                        <option>1975</option>
+	      				<option>1976</option>
+                        <option>1977</option>
+	      				<option>1978</option>
+                        <option>1979</option>
+	      				<option>1980</option>
+	      				<option>1981</option>
+                        <option>1982</option>
+	      				<option>1983</option>
+                        <option>1984</option>
+	      				<option>1985</option>
+                        <option>1986</option>
+	      				<option>1987</option>
+                        <option>1988</option>
+	      				<option>1989</option>
+                        <option>1988</option>
+	      				<option>1989</option>
+                        <option>1988</option>
+	      				<option>1989</option>
+	      				<option>1990</option>
+                        <option>1991</option>
+	      				<option>1992</option>
+	      				<option>1993</option>
+                        <option>1994</option>
+	      				<option>1995</option>
+	      				<option>1996</option>
+                        <option>1997</option>
+	      				<option>1998</option>
+	      				<option>1999</option>
+                        <option>2000</option>
+	      				<option>2001</option>
+	      				<option>2002</option>
+	      				<option>2003</option>
+	      				<option>2004</option>
+	      				<option>2005</option>
+                        <option>2006</option>
+	      				<option>2006</option>
+	      				<option>2007</option>
+                        <option>2008</option>
+	      				<option>2009</option>
+	      				<option>2010</option>
 	      				<option>2011</option>
+	      				<option>2012</option>
+	      				<option>2013</option>
 	      			</select>
 	    		</div>
 	  		</div>
@@ -104,7 +151,7 @@
 
 	  		<div class="control-group span10 offset1">	  		
 	    			Comentarios del Proceso
-	      			<textarea class="span9" disabled></textarea>
+                    <textarea class="span9" disabled></textarea>
 	  		</div>	  		
 	  	</fieldset>  		  		  		
     	<fieldset class="well">  		  	
--- application/views/prima/reportes.php
+++ application/views/prima/reportes.php
@@ -1,5 +1,27 @@
 <div class="row">
   <div class="span12 offset1">
-  	Reportes
+  <?php echo form_open('',array('class'=>'form-horizontal'));?>
+  <fieldset class="well">
+  		<div class="control-group span10">
+	    		<label class="control-label" for="registro-patronal-5">Seleccione Reporte</label>
+	    		<div class="controls">
+	      			<select>
+	      				<option></option>
+                        <option name="trabajadores">Trabajadores Promedio Expuestos al Riesgo</option>
+	      				<option name="reporte">Reporte de Riesgos de Trabajo</option>
+                        <option name="caratula">Car&aacute;tula del Formulario</option>
+	      				<option name="relacion">Relaci&oacute;n de Casos de R.T.</option>
+                        <option name="incapacidad">Incapacidades de Trabajadores</option>
+                    </select>	  	
+                </div>
+	  		</div>
+            <div class="control-group span10 offset1">	  	
+	      			<textarea class="span9" disabled style="height:200px;"></textarea>
+	  		</div>
+            <fieldset class="well">  		  	
+	    		<button class="btn span3 offset1">Generar</button>    			
+	    		<button class="btn span3 offset3">Salir</button>
+    	</fieldset>
+
   </div>
 </div>
